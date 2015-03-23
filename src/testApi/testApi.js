3b3d5f0fd9f3b10112deb786fb3ec507e33c15cc
Mark Moissette <kaosat.dev@gmail.com>
2015-03-23 14:10:43 +0100

    - moved some more code from kernel to testApi

--- src/testApi/testApi.js
+++ src/testApi/testApi.js
@@ -18,6 +18,46 @@ class TestApi{
   
   loadDesign( uri, options ){
     console.log("loading design from", uri);
+    
+     //TODO: should look more like this
+    //let design = yield this.dataApi.loadDesign( uri , options ); 
+    
+    //this.loadActiveAssemblyState( );
+    //now get back mapping of fileName to uid
+    let meshNameToPartTypeUId = localStorage.getItem("jam!-meshNameToPartTypeUId" );
+    if( meshNameToPartTypeUId) {
+      meshNameToPartTypeUId = JSON.parse( meshNameToPartTypeUId );
+      this.partRegistry._meshNameToPartTypeUId = meshNameToPartTypeUId;
+      
+      //FIXME: horrible hack, see code in part registry
+      for( key in meshNameToPartTypeUId)
+      {
+        let klass = Part;
+        let cName = nameCleanup(key);
+        let options = { name:cName };
+        let part = new klass( options );//new Part( options );
+        let typeUid = meshNameToPartTypeUId[key];
+        
+        part.typeName = cName;//name of the part CLASS
+        part.typeUid  = typeUid;
+        this.partRegistry.parts[ typeUid ] = part;
+        
+        this.partRegistry.registerPartInstance( part );
+        //not sure
+        part.name = part.typeName + "" + (this.partRegistry.partTypeInstances[ typeUid ].length - 1);
+        
+        //do we have ANY meshes for this part ?
+        //if not, add it to templates
+      }
+    }
+    
+    
+    
+    //now that we have the list of files that we need, load those
+    this.loadDocsOfDesign( callback );
+    
+    return this.activeDesign;
+    
   }
   
   saveAssemblyState( ){
@@ -122,6 +162,35 @@ class TestApi{
     reader.readAsBinaryString(file);*/
   }
   
+   /* TODO: how about reusing the asset manager????
+    also : only load data for files actually use in assembly
+  */
+  loadFilesList( ){
+    let self = this;
+     let apiUri = "http://localhost:3080/api/";
+      let uri = `${apiUri}designs/${this.activeDesign.name}/documents`;
+      
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', uri, true);
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          console.log("fetched ok");
+          console.log(this.responseText);
+          let data = JSON.parse( this.responseText );
+          data = data.filter( function( entry ){
+            let ext = entry.split(".").pop();
+            return ( ext.toLowerCase() === "stl");
+          });
+          if( callback ) callback( data );
+          
+          self._designDocs = data;
+          
+        } else {
+          console.error('An error occurred!');
+        }
+      };
+      xhr.send();
+  }
   
 }
 
