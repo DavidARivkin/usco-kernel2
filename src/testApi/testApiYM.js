716d2de7fae8dbfe6d67dcd6d0152c49bc3638d9
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-05-14 21:34:37 +0200

    - started "quest to remove useless semicolumns"

--- src/testApi/testApiYM.js
+++ src/testApi/testApiYM.js
@@ -1,11 +1,11 @@
 import Rx from 'rx'
-let Observable= Rx.Observable;
-let fromPromise = Observable.fromPromise;
+let Observable= Rx.Observable
+let fromPromise = Observable.fromPromise
 
 import logger from 'log-minim'
 
-let log = logger("testApi");
-log.setLevel("debug");
+let log = logger("testApi")
+log.setLevel("debug")
 
 import {normalizeString} from '../utils'
 
@@ -17,7 +17,7 @@ function jsonToFormData(jsonData){
     let value = jsonData[fieldName]
     //value = encodeURIComponent(JSON.stringify(value))
     //value = JSON.stringify(value)
-    //value = value.replace(/\"/g, '');
+    //value = value.replace(/\"/g, '')
     if(Object.prototype.toString.call(value) === "[object Object]"){
       value = JSON.stringify(value)
     }
@@ -29,26 +29,26 @@ function jsonToFormData(jsonData){
 
 class TestApiYM{
   constructor(){
-    this.apiUri    = "http://localhost:3080/api/";
-    this.designsUri = this.apiUri+"designs/";
+    this.apiUri    = "http://localhost:3080/api/"
+    this.designsUri = this.apiUri+"designs/"
     
-    this.designsUri = "http://jamapi.youmagine.com/api/v1/designs/";
-    //let designUri = "http://jamapi.youmagine.com/api/v1/designs/test";
-    this.rootUri    = undefined;
-    this.designName = undefined;
+    this.designsUri = "http://jamapi.youmagine.com/api/v1/designs/"
+    //let designUri = "http://jamapi.youmagine.com/api/v1/designs/test"
+    this.rootUri    = undefined
+    this.designName = undefined
 
-    this.assembliesFileName = "assemblies.json";//"assemblies_old.json";//"assemblies-simple.json"//
-    this.bomFileName        = "bom.json";//"bom_old.json"//"bom.json";
+    this.assembliesFileName = "assemblies.json"//"assemblies_old.json"//"assemblies-simple.json"//
+    this.bomFileName        = "bom.json"//"bom_old.json"//"bom.json"
     //TODO: use our pre-exising "stores"
-    this._designDocs = [];
+    this._designDocs = []
 
-    //this.assetManager = undefined;
-    this.store = undefined;
+    //this.assetManager = undefined
+    this.store = undefined
   }
 
   //FIXME: not sure about this ?
   setDesignName(name){
-    this.rootUri = this.designsUri+"/"+name;
+    this.rootUri = this.designsUri+"/"+name
     //check if the name is taken ?
   }
 
@@ -56,59 +56,59 @@ class TestApiYM{
   loadFullDesign(designUri){
     log.info("loading design from uri",designUri)
     //check if design root exists
-    this.rootUri = designUri;
+    this.rootUri = designUri
 
     //fetch design.json
-    let $designMeta = this.loadDesignMeta();
+    let $designMeta = this.loadDesignMeta()
     
     //fetch bom.json
     let $bom = this.loadBom()
 
     //fetch assemblies.json
-    let $assembly = this.loadAssemblyState();
+    let $assembly = this.loadAssemblyState()
     
     //fetch mapping type->uid
-    //this.loadMeshNameToPartTypeUId();
+    //this.loadMeshNameToPartTypeUId()
 
     //fetch needed meshes
-    //let $meshes = this.loadMeshes();
+    //let $meshes = this.loadMeshes()
 
 
     function generateOutputData(designMeta, bom, assemblies){
-      //console.log(designMeta, bom, assemblies); 
+      //console.log(designMeta, bom, assemblies) 
       let output = {}
-      output.design = designMeta;
+      output.design = designMeta
       output.bom = bom
         //FIXME : remap because of current limitations
         .map( function(bomEntry) { 
           if(bomEntry.uuid){bomEntry.id= bomEntry.uuid}
 
           return bomEntry} )
-      output.assemblies = assemblies;
+      output.assemblies = assemblies
       return output
     }
 
     function getNeededMeshesData(data){
       //console.log(data)
-      let {design, bom, assemblies} = data;
+      let {design, bom, assemblies} = data
       //we only care about the first assembly
-      let assembly = assemblies[0];
+      let assembly = assemblies//[0]
       //for every item in the assembly, fetch the needed data
 
-      let output = Object.assign({}, data);
-      /*let meshUrls = [];
+      let output = Object.assign({}, data)
+      /*let meshUrls = []
       bom.forEach(function(bomEntry){
         if(bomEntry && bomEntry.implementations && bomEntry.implementations.default){
-          let meshFileUri = bomEntry.implementations.default;
+          let meshFileUri = bomEntry.implementations.default
           meshUrls.push( meshFileUri )
         }
       })
-      output._neededMeshUrls = meshUrls;*/
-      //let meshNameToTypeUidUri = {};
-      //this.partRegistry._meshNameToPartTypeUId = meshNameToTypeUidUri;
+      output._neededMeshUrls = meshUrls*/
+      //let meshNameToTypeUidUri = {}
+      //this.partRegistry._meshNameToPartTypeUId = meshNameToTypeUidUri
 
       //now that we have the list of files that we need, load those
-      return output;
+      return output
     }
 
     var source = Rx.Observable.combineLatest(
@@ -118,26 +118,20 @@ class TestApiYM{
         generateOutputData
     )
       .take(1)
-      .map(getNeededMeshesData);
+      .map(getNeededMeshesData)
 
-    return source;
+    return source
    
   }
 
   ///////////
   /* load a given assembly*/
   loadAssemblyState( ){
-    let assemblyUri = `${this.rootUri}/assemblies`
-
-    /*let defferred = store.read(fileOrFileName)
-    return defferred.promise.then(function(){
-      let strAssembly = JSON.parse( strAssembly );
-      return strAssembly
-    })*/
+    let assemblyUri = `${this.rootUri}/assemblies/default`
 
     //NOTE : this is a cancellable q deferred, not a promise
-    let rawAssemblyPromise = this.store.read(assemblyUri).promise;
-    //self.activeDesign.activeAssembly = new Assembly( strAssembly );
+    let rawAssemblyPromise = this.store.read(assemblyUri).promise
+    //self.activeDesign.activeAssembly = new Assembly( strAssembly )
     let $rawData = fromPromise(rawAssemblyPromise)
     return $rawData.map( data => JSON.parse( data) )
   }
@@ -170,13 +164,13 @@ class TestApiYM{
 
   /*load the deign metadata*/
   loadDesignMeta(){
-    let designUri = `${this.rootUri}`;
+    let designUri = `${this.rootUri}`
     log.info("Loading design meta from ",designUri)
 
     //NOTE : this is a cancellable q deferred, not a promise
-    let rawDesignMetaPromise = this.store.read(designUri).promise;
-    let $rawDesignMeta = fromPromise(rawDesignMetaPromise);
-    return $rawDesignMeta.map( data => JSON.parse( data) );
+    let rawDesignMetaPromise = this.store.read(designUri).promise
+    let $rawDesignMeta = fromPromise(rawDesignMetaPromise)
+    return $rawDesignMeta.map( data => JSON.parse( data) )
   }
 
   /*save the design metadata*/
@@ -238,34 +232,34 @@ class TestApiYM{
 
   /*load meshName -> partUid mapping*/
   loadMeshNameToPartTypeUId(){
-    let meshNameToTypeUidUri = `${this.rootUri}/bom.json`;
+    let meshNameToTypeUidUri = `${this.rootUri}/bom.json`
 
     //NOTE : this is a cancellable q deferred, not a promise
-    let rawMappingPromise = this.store.read(bomUri).promise;
-    let rawBom$ = fromPromise(rawBomPromise);
-    return rawBom$.map( data => JSON.parse( data) );
+    let rawMappingPromise = this.store.read(bomUri).promise
+    let rawBom$ = fromPromise(rawBomPromise)
+    return rawBom$.map( data => JSON.parse( data) )
   }
 
   /*load meshes*/
   loadMeshes(){
-    let bomUri = `${this.rootUri}/bom.json`;
+    let bomUri = `${this.rootUri}/bom.json`
     log.info("Loading bom from ",bomUri)
 
     //NOTE : this is a cancellable q deferred, not a promise
-    let rawBomPromise = this.store.read(bomUri).promise;
-    let rawBom$ = fromPromise(rawBomPromise);
-    return rawBom$.map( data => JSON.parse( data) );
+    let rawBomPromise = this.store.read(bomUri).promise
+    let rawBom$ = fromPromise(rawBomPromise)
+    return rawBom$.map( data => JSON.parse( data) )
   }
 
   /*save a file/document at the given path*/
   saveFile( path, data ){
     if(!this.rootUri){
       log.info("not rootUri specified, cannot save file")
-      return;
+      return
     }
     log.info("saving file to",path)
 
-    let fileName = path;
+    let fileName = path
     let cleanedFileName = normalizeString(path)//path.toLowerCase().replace(/\./g, '-')
     //let fileUri = "http://jamapi.youmagine.com/api/v1/designs/test/documents/"+cleanedFileName
     let fileUri = `${this.rootUri}/documents/${cleanedFileName}`
@@ -275,7 +269,7 @@ class TestApiYM{
       modelfile:data
     }
 
-    uploadData = new FormData();
+    uploadData = new FormData()
     uploadData.append("modelfile", data)
 
     let rawDef = this.store.write(fileUri, uploadData )//, {formatter:jsonToFormData})
@@ -328,15 +322,15 @@ class TestApiYM{
 
   //FIMXE : redundance with main app
   loadMesh(uriOrData, options){
-    let resource = this.assetManager.load( uriOrData, {keepRawData:true, parsing:{useWorker:true,useBuffers:true} } );
-    var source = fromPromise(resource.deferred.promise);
+    let resource = this.assetManager.load( uriOrData, {keepRawData:true, parsing:{useWorker:true,useBuffers:true} } )
+    var source = fromPromise(resource.deferred.promise)
   }
 
   /*save association between mesh Name and type uid*/
   saveMeshNameToPartTypeUId(meshNameToPartTypeUId){
     //TODO: store this resourceName/URI ==> uid on the server somewhere
-    let meshNameToPartTypeUIdMapStr = JSON.stringify( meshNameToPartTypeUId );
-    localStorage.setItem("jam!-meshNameToPartTypeUId", meshNameToPartTypeUIdMapStr );
+    let meshNameToPartTypeUIdMapStr = JSON.stringify( meshNameToPartTypeUId )
+    localStorage.setItem("jam!-meshNameToPartTypeUId", meshNameToPartTypeUIdMapStr )
   }
 
   /*fetch all of the designs of a given user*/
@@ -345,12 +339,12 @@ class TestApiYM{
   }
   
   loadDesign( uri, options ){
-    console.log("loading design from", uri);
+    console.log("loading design from", uri)
      
     //now that we have the list of files that we need, load those
-    this.loadDocsOfDesign( callback );
+    this.loadDocsOfDesign( callback )
     
-    return this.activeDesign;
+    return this.activeDesign
     
   }
   
@@ -358,37 +352,37 @@ class TestApiYM{
  
   /* temporary only */
   saveCustomEntityTypes( typesData ){
-    console.log("saving custom entity types",typesData);
+    console.log("saving custom entity types",typesData)
   } 
     
    /* TODO: how about reusing the asset manager????
     also : only load data for files actually use in assembly
   */
   loadFilesList( ){
-    let self = this;
-     let apiUri = "http://localhost:3080/api/";
-      let uri = `${apiUri}designs/${this.activeDesign.name}/documents`;
+    let self = this
+     let apiUri = "http://localhost:3080/api/"
+      let uri = `${apiUri}designs/${this.activeDesign.name}/documents`
       
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', uri, true);
+      var xhr = new XMLHttpRequest()
+      xhr.open('GET', uri, true)
       xhr.onload = function () {
         if (xhr.status === 200) {
-          console.log("fetched ok");
-          console.log(this.responseText);
-          let data = JSON.parse( this.responseText );
+          console.log("fetched ok")
+          console.log(this.responseText)
+          let data = JSON.parse( this.responseText )
           data = data.filter( function( entry ){
-            let ext = entry.split(".").pop();
-            return ( ext.toLowerCase() === "stl");
-          });
-          if( callback ) callback( data );
+            let ext = entry.split(".").pop()
+            return ( ext.toLowerCase() === "stl")
+          })
+          if( callback ) callback( data )
           
-          self._designDocs = data;
+          self._designDocs = data
           
         } else {
-          console.error('An error occurred!');
+          console.error('An error occurred!')
         }
-      };
-      xhr.send();
+      }
+      xhr.send()
   }
 }
 
