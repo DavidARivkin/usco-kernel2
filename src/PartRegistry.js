2cbb79769191ac94e1c339e9c38abeaf7bc048d1
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2015-05-15 15:42:46 +0200

    - added workaround to ensure each mesh instance has a different material (& thus color too) - removed semiColumns & a bit of cleanup

--- src/PartRegistry.js
+++ src/PartRegistry.js
@@ -1,7 +1,13 @@
 import Q from 'q'
 import co from 'co'
-import { generateUUID, hashCode, nameCleanup } from "./utils";
-import Part from "./Part";
+import { generateUUID, hashCode, nameCleanup } from "./utils"
+import Part from "./Part"
+
+import logger from 'log-minim'
+
+let log = logger("PartRegistry")
+log.setLevel("debug")
+
 
 //FIXME: how much of an overlap with bom ?
 //FIXME: how much of an overlap with asset manager?
@@ -53,12 +59,12 @@ class PartRegistry{
   addTemplateMeshForPartType( mesh, typeUid ){
     if( ! this._partTypeMeshTemplates[ typeUid ] ){
       
-      this._partTypeMeshTemplates[ typeUid ] = mesh;
+      this._partTypeMeshTemplates[ typeUid ] = mesh
       
       //anybody waiting for that mesh yet ?
       if( this._partTypeMeshWaiters[ typeUid ] ){
-        console.log("resolving mesh of ", typeUid );
-        this._partTypeMeshWaiters[ typeUid ].resolve( mesh );
+        console.log("resolving mesh of ", typeUid )
+        this._partTypeMeshWaiters[ typeUid ].resolve( mesh )
       }
     }
   }
@@ -69,7 +75,7 @@ class PartRegistry{
     @returns: a class
   */
   registerPartTypeMesh( partKlass, mesh, options ){
-    console.log("registering part mesh");
+    console.log("registering part mesh")
     
     /*we also check if the implementation (the stl, amf etc) file is already registered 
     as an implementation of some part type*/
@@ -77,42 +83,42 @@ class PartRegistry{
     //the options are actually for the MESH 
     //we get the name of the mesh (needed)
     //we do not want the mesh instance to have the name of the mesh file
-    let meshName = options.name || "";
-    let cName = nameCleanup( meshName ); 
+    let meshName = options.name || ""
+    let cName = nameCleanup( meshName ) 
     
-    let typeUid = this._meshNameToPartTypeUId[ meshName ];
+    let typeUid = this._meshNameToPartTypeUId[ meshName ]
     
     //no typeUid was given, it means we have a mesh with no part (yet !)
     if( !typeUid ) {
       //FIXME: instead of PART , it could be a custom class created on the fly
-      let klass = Part;
-      typeUid = generateUUID();
+      let klass = Part
+      typeUid = generateUUID()
       //create ...
-      partKlass = this.makeNamedPartKlass( cName, typeUid );
+      partKlass = this.makeNamedPartKlass( cName, typeUid )
       //& register class
-      this.registerPartType( partKlass, cName, typeUid );
-      //this.partTypes[ typeUid ]     = partKlass;
-      //this.partTypesByName[ cName ] = partKlass;
+      this.registerPartType( partKlass, cName, typeUid )
+      //this.partTypes[ typeUid ]     = partKlass
+      //this.partTypesByName[ cName ] = partKlass
       
       //TODO: move this to a visual specific part of the code
-      this._meshNameToPartTypeUId[ meshName ] = typeUid;
+      this._meshNameToPartTypeUId[ meshName ] = typeUid
     }else{
-      partKlass = this.partTypes[ typeUid ];
+      partKlass = this.partTypes[ typeUid ]
     } 
     
     //do we have ANY meshes for this part ?
     //if not, add it to templates
-    this.addTemplateMeshForPartType( mesh.clone(), typeUid );
+    this.addTemplateMeshForPartType( mesh.clone(), typeUid )
 
-    return {partKlass,typeUid};
+    return {partKlass,typeUid}
   }
   
   /* register a part type
   */
   registerPartType( partKlass, typeName, typeUid ){
-      if( !partKlass ) throw new Error("no part type specified, cannot register part type");
-      this.partTypes[ typeUid ]     = partKlass;
-      this.partTypesByName[ typeName ] = partKlass;
+      if( !partKlass ) throw new Error("no part type specified, cannot register part type")
+      this.partTypes[ typeUid ]     = partKlass
+      this.partTypesByName[ typeName ] = partKlass
   }
   
   /* register a part type's (parametric) source
@@ -122,93 +128,98 @@ class PartRegistry{
   
   /* register a part instance */
   registerPartInstance( partInstance ){
-    if( !partInstance ) throw new Error("no part specified, cannot register part");
-    //this.parts.push( part );
-    let typeUid = partInstance.typeUid;
+    if( !partInstance ) throw new Error("no part specified, cannot register part")
+    //this.parts.push( part )
+    let typeUid = partInstance.typeUid
     if( !this.partTypeInstances[ typeUid ] )
     {
-      this.partTypeInstances[ typeUid ] = [];
+      this.partTypeInstances[ typeUid ] = []
     }
-    this.partTypeInstances[ typeUid ].push( partInstance );
+    this.partTypeInstances[ typeUid ].push( partInstance )
   }
   
   /*experimental:
    generate a named subclass of part, based on the name of Part CLASS
   */
   makeNamedPartKlass( klassName, typeUid ){
-    console.log("making named class");
+    console.log("making named class")
     //FIXME : won't work, as class is not support in browsers , where the eval() is taking
     //place
     /*let expSubClassStr = `class ${klassName} extends Part{
         constructor( options ){
-          super( options );
+          super( options )
         }
-    }`;*/
+    }`*/
     //SO we use the "old" es5 syntax
     let expSubClassStr = `var ${klassName} = function(options){
-        Part.call( this, options );
+        Part.call( this, options )
       }
-      ${klassName}.prototype = Object.create( Part.prototype );
-      ${klassName}.prototype.constructor = ${klassName};  
+      ${klassName}.prototype = Object.create( Part.prototype )
+      ${klassName}.prototype.constructor = ${klassName}  
       
       //set class attributes
-      /*${klassName}.prototype.typeName = '${klassName}';
-      ${klassName}.prototype.typeUid = '${typeUid}';*/
-    `;
-    console.log("expSubClassStr",expSubClassStr);
-    let klass = eval( expSubClassStr );
-    klass.prototype.typeName = klassName;
-    klass.prototype.typeUid = typeUid;
+      /*${klassName}.prototype.typeName = '${klassName}'
+      ${klassName}.prototype.typeUid = '${typeUid}'*/
+    `
+    
+    let klass = eval( expSubClassStr )
+    klass.prototype.typeName = klassName
+    klass.prototype.typeUid = typeUid
     
     //what do we need this for ???
-    this._customPartTypesMeta[ typeUid ] = { typeName:klassName, typeUid:typeUid };
+    this._customPartTypesMeta[ typeUid ] = { typeName:klassName, typeUid:typeUid }
     
     
-    return klass;
+    return klass
   }
   
   /* create a new instance of a type */
   createTypeInstance( klassOrKlassName, options ){
-    //let klass = this.partTypesByName[ klassName ];
-    let klass = klassOrKlassName;
-    let typeUid = klass.prototype.typeUid;
-    var part = new klass( options );
+    //let klass = this.partTypesByName[ klassName ]
+    let klass = klassOrKlassName
+    let typeUid = klass.prototype.typeUid
+    var part = new klass( options )
     
     //Register instance
-    this.registerPartInstance( part );
+    this.registerPartInstance( part )
     //not sure
-    part.name = part.typeName + "" + (this.partTypeInstances[ typeUid ].length - 1);
-    return part;
+    part.name = part.typeName + "" + (this.partTypeInstances[ typeUid ].length - 1)
+    return part
   }
   
     
   /* wrapper abstracting whether one needs to wait for the part's mesh or not
   */
   *getPartTypeMesh( typeUid, original=false ){
-    if(!typeUid) throw new Error("no typeUid specified");
+    if(!typeUid) throw new Error("no typeUid specified")
     if( ! this._partTypeMeshTemplates[ typeUid ] && ! this._partTypeMeshWaiters[ typeUid ] ){
-      throw new Error(`No matching mesh found for type : ${typeUid}`);
+      throw new Error(`No matching mesh found for type : ${typeUid}`)
     }
     
     if( ! this._partTypeMeshWaiters[ typeUid ] ) {
-      this._partTypeMeshWaiters[ typeUid ] = Q.defer();
+      this._partTypeMeshWaiters[ typeUid ] = Q.defer()
     }
     
     if( this._partTypeMeshTemplates[ typeUid ] ){
-      let mesh = this._partTypeMeshTemplates[ typeUid ] ;
-      //console.log("mesh", mesh);
-      this._partTypeMeshWaiters[ typeUid ].resolve( mesh );
-      //let mesh = yield this._partTypeMeshWaiters[ typeUid ];
+      let mesh = this._partTypeMeshTemplates[ typeUid ] 
+      //console.log("mesh", mesh)
+      this._partTypeMeshWaiters[ typeUid ].resolve( mesh )
+      //let mesh = yield this._partTypeMeshWaiters[ typeUid ]
     }
     
-    //partMeshesToWaitFor.push( self.partWaiters[ typeUid ].promise );
-    let mesh = yield this._partTypeMeshWaiters[ typeUid ].promise;
+    //partMeshesToWaitFor.push( self.partWaiters[ typeUid ].promise )
+    let mesh = yield this._partTypeMeshWaiters[ typeUid ].promise
     //we have not been asked for the original mesh, get a copy instance
     if( !original ){
-      mesh = mesh.clone();
+      log.info("getting a clone")
+
+      let newMaterial = mesh.material.clone()
+      mesh = mesh.clone()
+      mesh.material = newMaterial
+
     }
     return mesh
   }
 }
 
-export default PartRegistry ;
+export default PartRegistry 
